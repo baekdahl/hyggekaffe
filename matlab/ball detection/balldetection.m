09f5af3e9fd9b17c85c8707ad653fa7fe40c6c91
simonhave <simonhartmannhave@gmail.com>
2011-02-25 15:45:25 +0100

    dfg

--- matlab/ball detection/balldetection.m
+++ matlab/ball detection/balldetection.m
@@ -1,33 +1,72 @@
 clc
 clear
-d = dir('c:/hyggekaffe/pool/balls/*.jpg');
 
-for k=1:length(d),
-    fname=d(k).name;
-    Image = imread(fname);
-    Image_HSV = rgb2hsv(Image);
-    fname
-    h = double(Image_HSV(:,:,1))*255;
-    s = double(Image_HSV(:,:,2))*255;
+d = dir('c:/hyggekaffe/pool/balls/');
+bins = 255;
+
+for j=3:length(d),
     
-    bins = 51;
+    if isdir(d(j).name),
     
-    hist_h = hist(h,bins);
-    hist_s = hist(s,bins);
+    subdir = dir(d(j).name);
+    subdirname = d(j).name
     
-    for i=1:bins,
-       histograms(k).h(i) = sum(hist_h(i,:));
-       histograms(k).s(i) = sum(hist_s(i,:));
+    histograms(j).h = zeros(1,bins);
+    histograms(j).s = zeros(1,bins);
+                
+        for k=3:length(subdir),
+
+            file = subdir(k).name
+
+            Image = imread([subdirname '/' file]);
+            Image_HSV = rgb2hsv(Image);
+
+            hist_h = hist(double(Image_HSV(:,:,1)),bins);
+            hist_s = hist(double(Image_HSV(:,:,2)),bins);
+       
+            for i=1:bins,
+               histograms(j).h(i) = histograms(j).h(i) + sum(hist_h(i,:));
+               histograms(j).s(i) = histograms(j).s(i) + sum(hist_s(i,:));
+            end
+        
+        end
+       
+        max_h = max(histograms(j).h);
+        max_s = max(histograms(j).s);
+        
+        histograms(j).h  = histograms(j).h / max_h;
+        histograms(j).s  = histograms(j).s / max_s;
+        
     end
+    
 end
 
+for k=3:18,
+    subplot(4,4,k-2);
+    
+    hold on
+    bar(histograms(k).h);
 
-for i=1:bins,
-    distance(i) = histograms(1).h(i) - histograms(2).h(i);
+    h = findobj(gca,'Type','patch');
+    set(h,'FaceColor','g','EdgeColor','g');
+    
+    plot(histograms(k).s);
+    
+    %h = findobj(gca,'Type','patch');
+    %set(h(2),'FaceColor','g','EdgeColor','g');
+    
+    hold off
+    
+    title(d(k).name,'Color','w');
 end
 
-SSE = distance.^2;
-SSE = sum(sqrt(SSE));
+% 
+% for i=1:bins,
+%     distance(i) = histograms(1).h(i) - histograms(3).h(i);
+% end
+% 
+% SSE = distance.^2;
+% SSE = sum(sqrt(SSE));
 
-clear hist_h hist_s k i n bins Image Image_HSV d fname k ; 
+clear hist_h hist_s k i n bins Image Image_HSV fname k subdir subdirname j file d ; 
 
