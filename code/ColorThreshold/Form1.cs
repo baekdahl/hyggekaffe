aabbf29bab0b29ca46b5d185b035f495cddeca0d
Jesper Baekdahl <jeb@skoleelever.dk>
2011-03-25 11:49:40 +0100

    Første forsøg med adaptive ball detection

---
+++ code/ColorThreshold/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace PoolTracker
+{
+    public partial class Form1 : Form
+    {
+        Capture capture = new Capture();
+        Image<Bgr, byte> cameraImage;
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        public void findBalls()
+        {
+            Image<Bgr, byte> tableImg = new Image<Bgr, byte>("tables/3.jpg");
+
+            PoolTable table = new PoolTable(tableImg);
+            imageBoxTable.Image = tableImg;
+
+            Point ballPos;
+            //foreach (string filename in Directory.GetFiles("balls", "*.jpg"))
+            for (int i=1; i <= 16; i++)
+            {
+                if (i == 16) i = 0;
+                string filename = "balls/" + i + ".jpg";
+                logMessage("Matching: " + filename);
+                Image<Bgr, byte> ball = new Image<Bgr, byte>(filename);
+                ballPos = table.findBall(ball);
+                imageBox1.Image = table.backProjectShow;
+                
+                Image<Gray, Byte> tableMatchMask = table._tableMatchMask.Copy();
+                tableMatchMask._EqualizeHist();
+                imageBox2.Image = tableMatchMask;
+
+                logMessage("Score: " + table.bestMatch + Environment.NewLine);
+                
+                drawBallPos(imageBoxTable.Image.Bitmap, new Rectangle(ballPos.X, ballPos.Y, table.ballDia, table.ballDia));
+                
+                this.Refresh();
+                Application.DoEvents();
+
+                if (i == 0) break; 
+            }
+        }
+
+        public void locateBalls()
+        {
+            Image<Bgr, byte> tableImg = new Image<Bgr, byte>("tables/1.jpg");
+
+            PoolTable table = new PoolTable(tableImg);
+            imageBoxTable.Image = tableImg;
+
+            Point ballPos;
+            //foreach (string filename in Directory.GetFiles("balls", "*.jpg"))
+            for (int i = 1; i <= 16; i++)
+            {
+                if (i == 16) i = 0;
+                string filename = "balls/bg.jpg";
+                logMessage("Matching: " + filename);
+                Image<Bgr, byte> ball = new Image<Bgr, byte>(filename);
+                ballPos = table.findBall(ball);
+                imageBox1.Image = table.backProjectShow;
+
+                Image<Gray, Byte> tableMatchMask = table._tableMatchMask.Copy();
+                tableMatchMask._EqualizeHist();
+                imageBox2.Image = tableMatchMask;
+
+                logMessage("Score: " + table.bestMatch + Environment.NewLine);
+
+                drawBallPos(imageBoxTable.Image.Bitmap, new Rectangle(ballPos.X, ballPos.Y, table.ballDia, table.ballDia));
+
+                this.Refresh();
+                Application.DoEvents();
+
+                if (i == 0) break;
+            }
+        }
+
+        public void locateBalls2()
+        {
+            imageBoxTable.Image = cameraImage;
+
+            Image<Bgr, byte> tableImg = cameraImage.Copy(new Rectangle(new Point(40, 280), new Size(780, 380)));
+            imageBox1.Image = tableImg;
+
+            PoolTable table = new PoolTable(tableImg);
+
+            List<Ball> balls = table.findBalls(16);
+
+            Image<Gray, Byte> tableMatchMask = table._tableMatchMask.Copy();
+            tableMatchMask._EqualizeHist();
+            imageBox2.Image = tableMatchMask;
+
+            foreach (Ball ball in balls)
+            {
+                drawBallPos(imageBox1.Image.Bitmap, new Rectangle(ball.position.X, ball.position.Y, table.ballDia, table.ballDia));
+            }
+       }
+
+
+        public void drawBallPos(Bitmap bitmap, Rectangle rect)
+        {
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Pen myPen = new Pen(System.Drawing.Color.Red, 3);
+            graphics.DrawEllipse(myPen, rect);
+        }
+
+        public void logMessage(string message)
+        {
+            textBoxLog.AppendText(message + Environment.NewLine);
+            textBoxLog.ScrollToCaret();
+        }
+
+        private void startCapture()
+        {
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 960);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 1280);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS, 100);
+            Application.Idle += new EventHandler(delegate(object sender, EventArgs e)
+            {  //run this until application closed (close button click on image viewer)
+                cameraImage = capture.QueryFrame(); //draw the image obtained from camera
+                locateBalls2();
+            });
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            startCapture();
+        }
+    }
+}
---
+++ code/ColorThreshold/PoolTable.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using Emgu.CV.CvEnum;
+using System.Diagnostics;
+
+namespace PoolTracker
+{
+    class PoolTable
+    {
+        //Histogram comparison options:
+        public static HISTOGRAM_COMP_METHOD comparisonMethod = HISTOGRAM_COMP_METHOD.CV_COMP_INTERSECT;
+        public static int[] histBins = new int[] { 16, 16 };
+        public static RangeF[] histRanges = new RangeF[] { new RangeF(0, 180), new RangeF(0, 255) };
+        
+        private Image<Bgr, byte> _tableImage;
+        private Image<Hsv, byte> _tableImageHsv;
+        private Image<Gray, byte>[] _tablePlanes;
+        public Image<Gray, byte> _tableMatchMask;
+        public int ballDia = 26;
+        
+
+        public float bestMatch = 0;
+
+        public Image<Gray, byte> backProjectShow;
+
+        public PoolTable(Image<Bgr, byte> tableImage)
+        {
+            _tableImage = tableImage;
+            _tableImageHsv = tableImage.Convert<Hsv, byte>();
+            _tablePlanes = _tableImageHsv.Split();
+            _tableMatchMask = ImageUtil.thresholdAdaptiveMax(_tablePlanes[0], 1);
+        }
+
+        public Point findBall(Image<Bgr, byte> ballImage)
+        {
+            return findBall(ballImage.Convert<Hsv,Byte>());
+        }
+
+        public Point findBall(Image<Hsv, byte> ballImage)
+        { 
+            DenseHistogram ballHist = new DenseHistogram(histBins, histRanges);
+            Image<Gray, byte>[] ballPlanes = ballImage.Split();
+
+            float ballRadius = ballDia/2;
+            Image<Gray, byte> ballMask = new Image<Gray, byte>(ballDia, ballDia, new Gray(0));
+
+            ballMask.Draw(new CircleF(new PointF(ballRadius, ballRadius), ballRadius), new Gray(1), -1);
+
+            ballHist.Calculate<byte>(new Image<Gray, byte>[] { ballPlanes[0], ballPlanes[1] }, false, ballMask);
+
+            //Image<Gray, float> backProjection = ballHist.BackProjectPatch<byte>(new Image<Gray, byte>[] { _tablePlanes[0], _tablePlanes[1] }, new Size(35, 35), comparisonMethod, 1);
+            Image<Gray, float> backProjection = ImageUtil.backProjectPatchMasked(ballHist, new Image<Gray, byte>[] { _tablePlanes[0], _tablePlanes[1] }, ballMask, comparisonMethod, _tableMatchMask);
+           
+            double[] min, max;
+            Point[] minLoc, maxLoc;
+            Point ballPos;
+
+            backProjection.MinMax(out min, out max, out minLoc, out maxLoc);
+
+            ballPos = ImageUtil.matchHigh(comparisonMethod) ? maxLoc[0] : minLoc[0];
+            bestMatch = ImageUtil.matchHigh(comparisonMethod) ? (float)max[0] : (float)min[0];
+            
+            backProjectShow = backProjection.Convert<Gray, byte>();
+            backProjectShow._EqualizeHist();
+
+            _tableMatchMask.Draw(new CircleF(ballPos, ballDia - 5), new Gray(0), -1);
+
+            return ballPos;
+
+        }
+
+        private Size backProjectSize()
+        {
+            return new Size(_tableImage.Width - ballDia + 1, _tableImage.Height - ballDia + 1);
+        }   
+
+        private DenseHistogram backgroundHist()
+        {
+            Ball backgroundBall = new Ball(new Image<Hsv, byte>("balls/bg.jpg"), ballDia);
+            return backgroundBall.histogram;
+        }
+
+        private DenseHistogram getBallHistogram(Point position)
+        {
+            Rectangle roi = new Rectangle(position, new Size(ballDia, ballDia));
+            DenseHistogram hist = new DenseHistogram(histBins, histRanges);
+
+            foreach (Image<Gray, byte> plane in _tablePlanes)
+            {
+                plane.ROI = roi;
+            }
+            hist.Calculate(new Image<Gray, byte>[] { _tablePlanes[0], _tablePlanes[1] }, false, Ball.getMask());
+            return hist;
+        }
+
+        public List<Ball> findBalls(int numberOfBalls)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Image<Gray, byte>[] planes = { _tablePlanes[0], _tablePlanes[1] };
+            Image<Gray, float> projection = ImageUtil.backProjectPatchMasked(backgroundHist(), planes, Ball.getMask(), comparisonMethod, _tableMatchMask);
+            sw.Stop();
+            Debug.Write("ProjTime:" + sw.ElapsedMilliseconds + Environment.NewLine);
+
+            bool matchHigh = ImageUtil.matchHigh(comparisonMethod);
+            List<Ball> returnList = new List<Ball>();
+
+            for (int i=0; i < numberOfBalls; i++) {
+                sw = Stopwatch.StartNew();
+                KeyValuePair<Point, float> extremum = ImageUtil.findExtremum(projection, _tableMatchMask, matchHigh);
+                returnList.Add(new Ball(getBallHistogram(extremum.Key), extremum.Key));
+                
+                //Mark area matched in mask
+                _tableMatchMask.Draw(new CircleF(extremum.Key, ballDia - 5), new Gray(0), -1);
+            }
+            return returnList;
+        }
+
+        public Image<Gray,float> adaptiveBallDetect(Image<Gray, byte> input)
+        {
+            int h_split = 10;
+            int v_split = 5;
+            int h_stepsize = input.Width / h_split;
+            int v_stepsize = input.Height / v_split;
+            Image<Gray, byte> template = new Image<Gray,byte>(ballDia+2, ballDia+2);
+
+            Image<Gray, float> img_out = input.CopyBlank();
+
+            for (int h = 0; h < h_split; h++)
+            {
+                for (int v = 0; v < v_split; v++)
+                {
+                    Rectangle ROI = new Rectangle(new Point(h_stepsize * h, v_stepsize * v), new Size(h_stepsize, v_stepsize));
+                    input.ROI = ROI;
+
+                    DenseHistogram hist = new DenseHistogram(255, new RangeF(0, 255));
+                    hist.Calculate(new Image<Gray, Byte>[] { input }, false, null);
+
+                    float maxValue = 0;
+                    float minValue = 0;
+                    int[] maxLocation = { 0 };
+                    int[] minLocation = { 0 };
+                    hist.MinMax(out minValue, out maxValue, out minLocation, out maxLocation);
+
+                    img_out.ROI = ROI;
+                    template.Draw(new CircleF(new PointF(ballDia, ballDia), ballDia/2), new Gray(maxLocation[0]), -1);
+                    input.MatchTemplate(template, TM_TYPE.CV_TM_CCOEFF).CopyTo(img_out);
+                }
+            }
+            img_out.ROI = Rectangle.Empty;
+            input.ROI = Rectangle.Empty;
+
+            return img_out;
+        }
+    }
+    }
+}
