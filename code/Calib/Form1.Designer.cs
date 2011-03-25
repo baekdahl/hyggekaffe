20c2b5371b2d08462a693895e490ffbaf6e41b9c
simonhave <simonhartmannhave@gmail.com>
2011-03-25 10:17:45 +0100

    asdfv

--- code/Calib/Form1.Designer.cs
+++ code/Calib/Form1.Designer.cs
@@ -28,66 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(9, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(33, 82);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(900, 506);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // pictureBox2
+            // tableLayoutPanel1
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(33, 594);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(776, 503);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.imageBox4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(567, 357);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(3, 23);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(277, 162);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox2.Location = new System.Drawing.Point(286, 23);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(278, 162);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox3.Location = new System.Drawing.Point(3, 191);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(277, 163);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox3.TabIndex = 5;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox4.Location = new System.Drawing.Point(286, 191);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(278, 163);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox4.TabIndex = 6;
+            this.imageBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 14);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 1006);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(567, 357);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Pool Calibration";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- code/Calib/Form1.cs
+++ code/Calib/Form1.cs
@@ -40,11 +40,13 @@ namespace Calib
     public partial class Form1 : Form
     {
         Point[] positions = new Point[0];
+        Image<Bgr, Byte> camera_image;
+        Rectangle imgRect = new Rectangle(new Point(38, 168), new Size(541, 278));
 
         public Form1()
         {
             InitializeComponent();
-            
+            /*
             DirectoryInfo di = new DirectoryInfo("c://hyggekaffe//pics//randx");
             FileInfo[] rgFiles = di.GetFiles("*.jpg");
             foreach (FileInfo fi in rgFiles)
@@ -82,7 +84,7 @@ namespace Calib
                 pictureBox2.Image = imageStruct.threshold_diamond.ToBitmap();
                 pictureBox1.Image = imageStruct.org_img.ToBitmap();
                 
-            }
+            }*/
         }
 
         private List<Contour<Point>> contourfind2(Image<Gray,Byte> img) {
@@ -192,8 +194,107 @@ namespace Calib
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Capture capture = new Capture(); //create a camera captue
-            Image<Bgr, Byte> img = capture.QueryFrame();
+            test();
+        }
+
+        private void test()
+        {
+            Capture capture = new Capture(); //create a camera capture
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 960/2);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 1280/2);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FPS, 100);
+
+
+            Application.Idle += new EventHandler(delegate(object sender, EventArgs e)
+            {  //run this until application closed (close button click on image viewer)
+                Image<Bgr, Byte> img = capture.QueryFrame(); //draw the image obtained from camera
+                camera_image = img.Copy(imgRect);
+                //pictureBox1.Image = img.Resize(0.5, INTER.CV_INTER_NN).ToBitmap();
+                test3();
+            });
+        }
+
+        private void test2()
+        {
+            calibImage imageStruct = new calibImage();
+
+            imageStruct.org_img = camera_image;
+            //imageStruct.org_img = imageStruct.org_img.Resize(1, INTER.CV_INTER_NN);
+            //imageStruct.org_img = removebackground(imageStruct.org_img);
+
+            imageStruct.threshold_diamond = imageStruct.org_img.Convert<Gray, Byte>();  //Convert to Gray for thresholding
+
+            CvInvoke.cvAdaptiveThreshold(imageStruct.threshold_diamond.Ptr, imageStruct.threshold_diamond.Ptr, //Adptive threshold
+                    255, Emgu.CV.CvEnum.ADAPTIVE_THRESHOLD_TYPE.CV_ADAPTIVE_THRESH_MEAN_C, Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY, 301, 10);
+
+            imageStruct.threshold_diamond = imageStruct.threshold_diamond.Dilate(2);    //Dialate
+            imageStruct.threshold_diamond = imageStruct.threshold_diamond.Erode(2);     //Erode
+
+            imageStruct.contourpoint = contourfind2(imageStruct.threshold_diamond);     //Find contours in image
+
+            double medianArea = imageStruct.contourpoint[(int)Math.Floor((double)imageStruct.contourpoint.Count / 2)].Area; //Find median size contours
+
+            foreach (Contour<Point> contour in imageStruct.contourpoint)    //Draw contours
+            {
+                if (Math.Abs(contour.Area - medianArea) < 50)
+                {
+                    Debug.Write("\n" + contour.Area + "\n");
+                    imageStruct.org_img.Draw(contour, new Bgr(0, 255, 255), 3);
+                    Debug.Write(contour.Area / Math.Pow(contour.Perimeter, 2));
+                }
+            }
+
+            //pictureBox4.Image = 
+            imageBox1.Image = imageStruct.threshold_diamond;
+            imageBox2.Image = imageStruct.org_img;
+
+        }
+
+        private void test3() {
+
+            Image<Gray,Byte>[] img_split = camera_image.Convert<Hsv,Byte>().Split();
+
+            img_split[0] = img_split[0].Resize(1, INTER.CV_INTER_NN);
+
+            int h_split = 10;
+            int v_split = 5;
+            int h_stepsize = img_split[0].Width / h_split;
+            int v_stepsize = img_split[0].Height / v_split;
+
+            Image<Gray,Byte> img_out = img_split[0].CopyBlank();
+
+            for (int h = 0; h < h_split; h++)
+            {
+                for (int v = 0; v < v_split; v++)
+                {
+                    Rectangle ROI = new Rectangle(new Point(h_stepsize * h, v_stepsize * v), new Size(h_stepsize, v_stepsize));
+                    img_split[0].ROI = ROI;
+
+                    DenseHistogram hist = new DenseHistogram(255, new RangeF(0, 255));
+                    hist.Calculate(new Image<Gray, Byte>[] { img_split[0] }, false, null);
+
+                    float maxValue = 0;
+                    float minValue = 0;
+                    int[] maxLocation = { 0 };
+                    int[] minLocation = { 0 };
+                    hist.MinMax(out minValue, out maxValue, out minLocation, out maxLocation);
+
+                    Image<Gray, Byte> thres_h11 = img_split[0].ThresholdBinary(new Gray(maxLocation[0] + 10), new Gray(255));
+                    Image<Gray, Byte> thres_h22 = img_split[0].ThresholdBinaryInv(new Gray(maxLocation[0] - 10), new Gray(255));
+                    
+                    img_out.ROI = ROI;
+                    thres_h11.Or(thres_h22).CopyTo(img_out);
+
+                }
+            }
+            img_out.ROI = Rectangle.Empty;
+            img_split[0].ROI = Rectangle.Empty;
+
+            imageBox1.Image = img_split[0];
+            imageBox2.Image = camera_image;
+            imageBox3.Image = img_out;
+            
+            
         }
 
     }    
