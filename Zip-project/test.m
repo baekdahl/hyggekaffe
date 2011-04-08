d6ead29ae6977eaf307cef0026f3f915abf89a4c
simonhave <simonhartmannhave@gmail.com>
2011-04-08 11:43:05 +0200

    zip projekt og mere rapport

--- Zip-project/test.m
+++ Zip-project/test.m
@@ -2,11 +2,11 @@ clc
 %clear
 
 %input_string = 'There have been some hair-raising goings-on outside the castle at Elsinore. As the terrified Horatio and Marcellus look on, the ghost of the recently deceased king appears to Prince Hamlet. The spirit beckons Hamlet offstage, and the frenzied prince follows after, ordering the witnesses to stay put.'
-input_string = 'sir sid eastman easily teases sea sick seals';
+%input_string = 'sir sid eastman easily teases sea sick seals';
 
 profile on;
 
-%input_string = import{(1)};
+input_string = import{(1)};
 
 [encoded_string dictionary] = encode(input_string);
 [p symbols] = frequency(encoded_string);
